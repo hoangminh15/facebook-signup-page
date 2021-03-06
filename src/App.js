52ae6b38d681d07118e210ff3b02a3ebf091eda5
hoangminh15 <hoangvanminhhust@gmail.com>
2021-03-06 16:53:15 +0700

    Done

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Footer from './Footer';
+import './LoginForm'
+import LoginForm from './LoginForm';
+import Main from './Main';
+
+function App() {
+  return (
+    <div className="app">
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
